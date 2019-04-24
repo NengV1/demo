a9ba5f778ed02577b98fd6d5017f69bdaa6405a2
nengonetwo@gmail.com <Neng12334>
2019-04-24 16:34:20 +0700

    dadadsasd

--- src/main/java/com/example/demo/entity/OleUser.java
+++ src/main/java/com/example/demo/entity/OleUser.java
@@ -8,5 +8,44 @@ import javax.persistence.Id;
 @Entity(name = "Oleuser")
 public class OleUser {
 
+    private Long id;
+    private String name;
+    private String teamName;
+    private Integer salary;
 
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
