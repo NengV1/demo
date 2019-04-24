88a5a1e0a98f580570754b9579775a524a124845
nengonetwo@gmail.com <Neng12334>
2019-04-24 15:45:37 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class ProductController {
+
+    @Autowired
+    UserRepository repo;
+
+    @GetMapping(value = "/all")
+    public List<User> getProduct(){
+
+        return repo.findAll();
+    }
+
+    @GetMapping(value ="/{name}")
+    public User findByName(@PathVariable final String name){
+        return repo.findByName(name);
+    }
+
+    @PostMapping(value = "/load")
+    public User load(@RequestBody final User product){
+        repo.save(product);
+        return repo.findByName(product.getName());
+
+    }
+
+}
+
