9bb89c39eb20db0da8428f7131e5d84956b33405
nengonetwo@gmail.com <Neng12334>
2019-04-24 16:37:33 +0700

    rename File

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
-public class ProductController {
+public class UserController {
 
     @Autowired
     UserRepository repo;
